7b695af8fb4a091cf3333d92a1d402ef348d1825
Sagar-57 <sagar05.ms@gmail.com>
2021-10-19 16:48:23 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './child-components/Header/header';
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      <Header/>
     </div>
   );
 }
